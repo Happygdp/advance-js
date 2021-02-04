d44b7b2fc5824201ed247cf77fb5007830daa5b6
happygdp <happygdp00966@gmail.com>
2021-02-04 09:56:43 -0800

    true vs false

---
+++ true-false.js
@@ -0,0 +1,19 @@
+// falsy
+// 0
+// ""
+// undefined
+// null
+// NaN
+// Truly
+// "0", " ", [],
+
+// const name = "0";
+// let name;
+let age = null;
+
+if(age){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
