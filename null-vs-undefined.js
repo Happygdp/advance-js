03a9994c0870220c52677624398f1f278e3c92c5
happygdp <happygdp00966@gmail.com>
2021-02-05 13:09:12 -0800

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,16 @@
+let pakhi;
+ function add(num1, num2){
+     console.log(num1 + num2);
+     return 
+    }
+
+ function add(num1, num2){
+     console.log(num1, num2);
+ }
+const result =add(13);
+console.log(result);
+let fun = undefined;
+console.log(fun);
+
+let ages = [5, 8, 4]
+console.log(ages);
